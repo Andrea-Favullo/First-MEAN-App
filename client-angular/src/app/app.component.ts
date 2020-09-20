9b0726ee866b141b78e101ea9ef976f153aa1367
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-09-20 19:40:37 +0000

    punto 3

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
+import { HttpClient } from '@angular/common/http'; //HTTP CLIENT
+
+import { Data } from './models/data.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +12,25 @@ import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
 export class AppComponent implements OnInit {
   results: string[];
   private BASE_URL: string = '/api';
+
+  obs: Observable<Data>;
+  url: string='https://3000-ba4f9bba-754f-4996-b5b5-2f81dbb0d058.ws-eu01.gitpod.io/api';
   // Inject HttpClient into your component or service.
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     // Make the HTTP request:
-    this.http.get('https://3000-afdf9a36-8b11-494d-b0be-e5b69c25083c.ws-eu01.gitpod.io/api').subscribe(data => {
+    this.obs = this.http.get<Data>(this.url);
+
+    this.obs.subscribe( data=>{
+      this.results = data['results'];
+      console.log(data)
+    } );
+    /*
+    this.http.get().subscribe(data => {
       // Read the result field from the JSON response.
       this.results = data['results'];
       console.log(data)
-    });
+    });*/
   }
 }
